9c4dde2f6c5297b7f2dbe5d396636a4b349323a9
root <root@adaa08250398>
2020-04-29 02:51:42 +0000

    spiral

--- src/section2/SmartShapes.java
+++ src/section2/SmartShapes.java
@@ -5,24 +5,32 @@ import org.jointheleague.graphical.robot.Robot;
 public class SmartShapes {
 	
     public static void main(String[] args) throws Exception {
-        // 1. Make a new Robot
+       
+    	
+    // 1. Make a new Robot
 
+    	Robot tod=new Robot();
 
         // 3. Put the robot's pen down
 
+    	tod.penDown();
+        
+	// 6. Make the robot move as fast as possible
 
-        // 6. Make the robot move as fast as possible
-
+tod.setSpeed(200);
 
         // 5. Use a for loop to repeat everything below 4 times. 
 
+for(int i=0; i<3; i++);
 
         //         2. Move your robot 200 pixels
 
+tod.move(200);
 
         //         4. Turn the robot 90 degrees to the right
 
-    	
+tod.turn(90);
+   	
     		// 7. Change steps 4 and 5 to draw a different shape (e.g. triangle (3-sides), pentagon (5-sides), decagon (10-sides)).
    
     }
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,14 +6,17 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot tod = new Robot();
 		// 5. Set your robot's pen down 
+		tod.actionPerformed(penDown();
 		
 		// 3. Set the robot to go at max speed (100)
-		
+		tod.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
-			// 7. Change the pen color to random
+		  for(int i=0; i<4; i++) {
+			
+				  
+				 // 7. Change the pen color to random
 		
 			// 6. Move the robot 5 times the loop counter (5*i)
 			
@@ -22,4 +25,4 @@ public class Spiral {
 			// 8. Set the pen width to i
 			
 	}
-}
+		  {
